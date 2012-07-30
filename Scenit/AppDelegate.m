60286a9b05f035f6bb87364c9eb53a86c926a0bb
Steven Bloemer <incredisteve@gmail.com>
2012-07-29 20:01:37 -0500

    Initial Commit

---
+++ Scenit/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Scenit
+//
+//  Created by Steven Bloemer on 7/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Scenit/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Scenit
+//
+//  Created by Steven Bloemer on 7/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
